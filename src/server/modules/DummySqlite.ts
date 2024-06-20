9b253e33eaab8e48b203c07383462ba8f7f40aa7
jaemin1005 <ghkdwoals1005@gmail.com>
2024-06-20 23:23:35 +0900

    Style | DummySqlite
    class 이름 변경

--- src/server/modules/DummySqlite.ts
+++ src/server/modules/DummySqlite.ts
@@ -1,9 +1,9 @@
 import { Sqlite } from "./DataBases/Sqlite";
 
-class DummyClass extends Sqlite {
+class DummyDB extends Sqlite {
   CreateTable(){
     this.ExecQuery('CREATE TABLE IF NOT EXISTS lorem (info TEXT)');
   }
 }
 
-const dummyDB = new DummyClass("Hello.db");
+const dummyDB = new DummyDB("Hello.db");
